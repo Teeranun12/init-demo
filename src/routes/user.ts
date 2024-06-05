e8ba77407e84d5b55c8fcad7379fc6a9d6ba0432
Teeranun_Bank <teeranun.s@beryl8.com>
2024-06-05 10:14:37 +0700

    add Todo

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,33 +2,42 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
+//TODO: add swagger
+/**
+ *
+ */
 router.get("/", (req: Request, res: Response) => {
   const data = {
-    "usersId": 20,
-    "name": "John",
-    "age": 30,
-    "city": "New York",
-    "country": "USA"
-}
+    usersId: 20,
+    name: "John",
+    age: 30,
+    city: "New York",
+    country: "USA",
+  };
 
   return res.send(data);
 });
 
+//TODO: add swagger
+
 router.post("/post", (req: Request, res: Response) => {
-  //add new user
+  //TODO : add new user
+
   return res.send("post");
 });
 
+//TODO: add swagger
+
 router.patch("/patch", (req: Request, res: Response) => {
   const data = {
-    "usersId": 20,
-    "first_name": "John",
-    "last_name": "doe",
-    "city": "New York",
-    "country": "USA"
-  }
+    usersId: 20,
+    first_name: "John",
+    last_name: "doe",
+    city: "New York",
+    country: "USA",
+  };
 
-  console.log(data)
+  console.log(data);
 
   //TODO: add update Data
   return res.send("patch");
